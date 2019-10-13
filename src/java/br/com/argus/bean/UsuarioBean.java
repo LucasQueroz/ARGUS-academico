0a0a836e118b4a33d678cebd1f03c38fd369cfdd
LucasQueroz <43549288+LucasQueroz@users.noreply.github.com>
2019-10-13 14:23:32 -0300

    Primeiro CRUD concluido

--- src/java/br/com/argus/bean/UsuarioBean.java
+++ src/java/br/com/argus/bean/UsuarioBean.java
@@ -12,11 +12,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import br.com.argus.model.Usuario;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ViewScoped;
 import javax.persistence.Query;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -66,20 +70,58 @@ public class UsuarioBean implements Serializable {
      
     }
     
-    public void editar(Long id){
+    /*public void editar(Long id){
         Usuario usuario = new Usuario();
         EntityManager e = JPAUtil.getEntityManagerFactory().createEntityManager();
         usuario = e.find(Usuario.class, id);
         //JPAUtil.shutdown();
         System.out.println("Usuario:"  + usuario.getNome());
+    }*/
+    
+    /*public static void main(String[] args) {
+        editar();
+    }*/
+    
+    public void editar(int id){
+        //JOptionPane.showMessageDialog(null, "Passei no editar com id:" + id);
+        
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        Usuario u = new Usuario();
+        u = usuarioDAO.buscar(id);
+        
+        Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+        sessionMap.put("usuario", u);
+        
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/editar_usuario.jsf");
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        //return "/editar_usuario.jsf";
     }
     
-    public void eliminar(Long id){
-        Usuario usuario = new Usuario();
+    public void atualizar(Usuario usuario){
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        usuarioDAO.editar(usuario);
+        
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("/listar_usuario.jsf");
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void eliminar(int id){
+        //JOptionPane.showMessageDialog(null, "Passei no eliminar com id: " + id);
+        /*Usuario usuario = new Usuario();
         EntityManager e = JPAUtil.getEntityManagerFactory().createEntityManager();
-        usuario = e.find(Usuario.class, id);
+        usuario = e.find(Usuario.class, id);*/
         //JPAUtil.shutdown();
-        System.out.println("Usuario:"  + usuario.getNome());
+        //System.out.println("Usuario:"  + usuario.getNome());
+        
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+        usuarioDAO.eliminar(id);
     }
 
     public Usuario getUsuario() {
--- src/java/br/com/argus/dao/UsuarioDAO.java
+++ src/java/br/com/argus/dao/UsuarioDAO.java
@@ -1,6 +1,37 @@
 package br.com.argus.dao;
 
+import br.com.argus.model.JPAUtil;
+import br.com.argus.model.Usuario;
+import javax.persistence.EntityManager;
+
 public class UsuarioDAO {
+    
+    EntityManager entity = JPAUtil.getEntityManagerFactory().createEntityManager();
+
+    public Usuario buscar(int id) {
+        /*Cliente c = new Cliente();
+	c = entity.find(Cliente.class, id);
+	// JPAUtil.shutdown();
+	return c;*/
+        
+        Usuario u = new Usuario();
+        u = entity.find(Usuario.class, id);
+        return u;
+    }
+    
+    public void eliminar(int id){
+        Usuario u = new Usuario();
+        u = entity.find(Usuario.class, id);
+        entity.getTransaction().begin();
+        entity.remove(u);
+        entity.getTransaction().commit();
+    }
+    
+    public void editar(Usuario usuario){
+        entity.getTransaction().begin();
+        entity.merge(usuario);
+        entity.getTransaction().commit();
+    }
     
 //    EntityManager entity = JPAUtil.getEntityManagerFactory().createEntityManager();
     
